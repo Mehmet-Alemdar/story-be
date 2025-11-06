33712e6288a160e943cac170adae7112236ea05e
mehmetalemdar <alemdarmehmet6@gmail.com>
2025-11-06 21:27:09 +0300

    Update default mongo url

--- mongo-connection.js
+++ mongo-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://mongodb/story'
+const connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017/story'
 
 mongoose.connect(connectionString)
 
